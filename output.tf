7d9a56c07a56b150bfe91533304b7bec431deced
ravimtestgit <vmetta2@illinois.edu>
2026-02-17 22:58:23 -0800

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
+output "bucket_region" {
+  description = "AWS region of the bucket"
+  value       = aws_s3_bucket.this.region
+}
+
+output "bucket_domain_name" {
+  description = "Domain name of the bucket"
+  value       = aws_s3_bucket.this.bucket_domain_name
+}
